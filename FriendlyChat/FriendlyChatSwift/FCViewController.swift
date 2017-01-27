b052ff4a63c2cf884f1bda7618c8d4b43f57ba8a
Amy <amycheong19@gmail.com>
2017-01-27 21:30:50 +0800

    [WIP] Configuring email and password auth

--- FriendlyChat/FriendlyChatSwift/FCViewController.swift
+++ FriendlyChat/FriendlyChatSwift/FCViewController.swift
@@ -53,9 +53,7 @@ class FCViewController: UIViewController, UINavigationControllerDelegate {
     // MARK: Life Cycle
     
     override func viewDidLoad() {
-        self.signedInStatus(isSignedIn: true)
-        
-        // TODO: Handle what users see when view loads
+        configureAuth()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -64,13 +62,39 @@ class FCViewController: UIViewController, UINavigationControllerDelegate {
     }
     
     // MARK: Config
-    
     func configureAuth() {
-        // TODO: configure firebase authentication
+
+        // listen for changes in the authorization state
+        _authHandle = FIRAuth.auth()?.addStateDidChangeListener { (auth: FIRAuth, user: FIRUser?) in
+            // refresh table data
+            self.messages.removeAll(keepingCapacity: false)
+            self.messagesTable.reloadData()
+            
+            // check if there is a current user
+            if let activeUser = user {
+                // check if the current app user is the current FIRUser
+                if self.user != activeUser {
+                    self.user = activeUser
+                    self.signedInStatus(isSignedIn: true)
+                    let name = user!.email!.components(separatedBy: "@")[0]
+                    self.displayName = name
+                }
+            } else {
+                // user must sign in
+                self.signedInStatus(isSignedIn: false)
+                self.loginSession()
+            }
+        }
     }
     
     func configureDatabase() {
-        // TODO: configure database to sync messages
+        ref = FIRDatabase.database().reference()
+        //Listen when child is added
+        ref.child("messages").observe(.childAdded) { (snapshot: FIRDataSnapshot) in
+            self.messages.append(snapshot)
+            self.messagesTable.insertRows(at:[ IndexPath(row: self.messages.count - 1 , section: 0)], with: .automatic)
+            self.scrollToBottomMessage()
+        }
     }
     
     func configureStorage() {
@@ -78,7 +102,9 @@ class FCViewController: UIViewController, UINavigationControllerDelegate {
     }
     
     deinit {
-        // TODO: set up what needs to be deinitialized when view is no longer being used
+        //To avoid memory lost 
+        ref.child("messages").removeObserver(withHandle: _refHandle)
+        FIRAuth.auth()?.removeStateDidChangeListener(_authHandle)
     }
     
     // MARK: Remote Config
@@ -109,7 +135,7 @@ class FCViewController: UIViewController, UINavigationControllerDelegate {
             backgroundBlur.effect = nil
             messageTextField.delegate = self
             
-            // TODO: Set up app to send and receive messages when signed in
+            configureDatabase()
         }
     }
     
@@ -122,6 +148,13 @@ class FCViewController: UIViewController, UINavigationControllerDelegate {
     
     func sendMessage(data: [String:String]) {
         // TODO: create method that pushes message to the firebase database
+        var mdata = data
+        //To know who send the messages
+        mdata[Constants.MessageFields.name] = displayName
+        // path: /messages/[auto_ID]
+        // childByAutoId : choronically sorted
+        ref.child("messages").childByAutoId().setValue(mdata)
+        
     }
     
     func sendPhotoMessage(photoData: Data) {
@@ -201,8 +234,13 @@ extension FCViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // dequeue cell
         let cell: UITableViewCell! = messagesTable.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
+        let messageSnapshot : FIRDataSnapshot = self.messages[indexPath.row]
+        let message = messageSnapshot.value as! [String: String]
+        let username = message[Constants.MessageFields.name] ?? "[username]"
+        let text = message[Constants.MessageFields.text] ?? "[message]"
+        cell.textLabel?.text = username + ":" + text
+        cell.imageView?.image = self.placeholderImage
         return cell!
-        // TODO: update cell to display message data
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
